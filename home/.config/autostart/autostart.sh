0c54570a76eda2e0953e7ceed8a591ef567c41de
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2025-08-06 19:21:30 +0600

    Update autostart config

--- home/.config/autostart/autostart.sh
+++ home/.config/autostart/autostart.sh
@@ -15,16 +15,21 @@ killall -q flameshot
 flameshot &
 
 # XFCE specific auto start commands.
-if [ "$GDMSESSION" == "xfce" ]; then
+if [ "$XDG_SESSION_DESKTOP" == "xfce" ]; then
   plank
 fi
 
 # BSPWM specific auto start commands.
-if [ "$GDMSESSION" == "bspwm" ]; then
+if [ "$XDG_SESSION_DESKTOP" == "bspwm" ]; then
   pgrep -x sxhkd > /dev/null || sxhkd &
 
   bash ~/.config/polybar/launch.sh &
 
   killall -q walgen
   walgen > /dev/null &
+
+  killall -q nm-applet
+  nm-applet &
+
+  xinput set-prop 10 313 1 1 &
 fi
