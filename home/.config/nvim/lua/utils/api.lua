9b3c262b6d0c6a064efa0e790a3d94c55a0744cf
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2025-08-10 22:39:59 +0600

    chore(nvim): Add utility types
    Added types for *map(), autocmd(), augroup(), highlight(), command(),
    etc.
    
    Organized code

--- home/.config/nvim/lua/utils/api.lua
+++ home/.config/nvim/lua/utils/api.lua
@@ -1,4 +1,46 @@
+-- TODO: Add descriptions.
+
 local map = vim.keymap.set
+
+---@param mode string
+---@param lhs string
+---@param rhs string|function
+---@param opts? vim.keymap.set.Opts
+local function noremap(mode, lhs, rhs, opts)
+    opts = opts or {}
+    local default_opts = { noremap = true }
+    map(mode, lhs, rhs, vim.tbl_extend("keep", default_opts, opts))
+end
+
+---@type utils.map.args
+function _G.nmap(lhs, rhs, opts)
+    map("n", lhs, rhs, opts)
+end
+
+---@type utils.map.args
+function _G.imap(lhs, rhs, opts)
+    map("i", lhs, rhs, opts)
+end
+
+---@type utils.map.args
+function _G.vmap(lhs, rhs, opts)
+    map("v", lhs, rhs, opts)
+end
+
+---@type utils.map.args
+function _G.nnoremap(lhs, rhs, opts)
+    noremap("n", lhs, rhs, opts)
+end
+
+---@type utils.map.args
+function _G.inoremap(lhs, rhs, opts)
+    noremap("i", lhs, rhs, opts)
+end
+
+---@type utils.map.args
+function _G.vnoremap(lhs, rhs, opts)
+    noremap("v", lhs, rhs, opts)
+end
 
 _G.autocmd = vim.api.nvim_create_autocmd
 
@@ -7,67 +49,12 @@ function _G.augroup(group_name)
     vim.api.nvim_create_augroup(group_name, { clear = true })
 end
 
----@param name string
----@param commands string | fun(args: vim.api.keyset.create_user_command.command_args)
----@param opts?  vim.api.keyset.user_command
-function _G.command(name, commands, opts)
-    vim.api.nvim_create_user_command(name, commands, opts or {})
-end
-
----@param mode string|table
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-local function noremap(mode, lhs, rhs, opts)
-    opts = opts or {}
-    local default_opts = { noremap = true }
-    map(mode, lhs, rhs, vim.tbl_extend("keep", default_opts, opts))
-end
-
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-function _G.nmap(lhs, rhs, opts)
-    map("n", lhs, rhs, opts)
-end
-
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-function _G.imap(lhs, rhs, opts)
-    map("i", lhs, rhs, opts)
-end
-
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-function _G.vmap(lhs, rhs, opts)
-    map("v", lhs, rhs, opts)
-end
-
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-function _G.nnoremap(lhs, rhs, opts)
-    noremap("n", lhs, rhs, opts)
-end
-
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-function _G.inoremap(lhs, rhs, opts)
-    noremap("i", lhs, rhs, opts)
-end
-
----@param lhs string
----@param rhs string|function
----@param opts table|nil
-function _G.vnoremap(lhs, rhs, opts)
-    noremap("v", lhs, rhs, opts)
-end
-
----@param group_name string
----@param options vim.api.keyset.highlight
+---@type utils.highlight.args
 function _G.highlight(group_name, options)
     vim.api.nvim_set_hl(0, group_name, options or {})
+end
+
+---@type utils.command.args
+function _G.command(name, commands, opts)
+    vim.api.nvim_create_user_command(name, commands, opts or {})
 end
